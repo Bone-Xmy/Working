596dfbb038bc6c81ace0719e088f8933904c7bec
bone <bone@xmy.com>
2018-07-29 20:29:49 +0800

    add checkSetFood

--- src/org/saas/app/action/checkSetFood.java
+++ src/org/saas/app/action/checkSetFood.java
@@ -1,7 +1,6 @@
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-
-import com.google.gson.JsonElement;
+ 
 import com.google.gson.JsonArray;
 import com.google.gson.JsonIOException;
 import com.google.gson.JsonObject;
@@ -14,45 +13,32 @@ public class checkSetFood{
 	public static void main(String[] args){
 		JsonParser parse = new JsonParser(); //创建Json解析器
 		ArrayList<String> foodKeys = new ArrayList<>();
-		ArrayList<String> foodNames = new ArrayList<>();
 		try{
-			JsonObject json = (JsonObject)parse.parse(new FileReader("F:\\Study\\SaaS\\saasJson\\0725\\src\\foodLst.cdata"));
+			JsonObject json = (JsonObject)parse.parse(new FileReader("F:\\Study\\SaaS\\saasJson\\src\\foodLst.cdata"));
 			
 			JsonArray array = json.get("data").getAsJsonObject().get("records").getAsJsonArray(); //获取foodLst数组
 			//维护foodKeys
 			for(int i = 0; i < array.size(); i++){
 				JsonObject foodLst = array.get(i).getAsJsonObject();
 				foodKeys.add(foodLst.get("foodKey").getAsString()); //获取菜品的foodKey，维护foodKeys
-				foodNames.add(foodLst.get("foodName").getAsString()); //获取菜品的foodName,维护foodNames
 			}
 			
 			for(int i = 0; i < array.size(); i++){
 				JsonObject foodLst = array.get(i).getAsJsonObject();
-				String foodName = foodLst.get("foodName").getAsString();
-				JsonElement setFoodLst_temp = foodLst.get("setFoodDetailJson");
-				//如果是JsonObject说明有套餐明细，是套餐
-				if(setFoodLst_temp.isJsonObject()){
-					JsonObject setFoodLst = setFoodLst_temp.getAsJsonObject();
+				JsonObject setFoodLst = foodLst.get("setFoodDetailJson").getAsJsonObject();
+				//如果有套餐明细，则获取套餐明细
+				if(setFoodLst != null){
 					JsonArray foodLstArray = setFoodLst.get("foodLst").getAsJsonArray();
 					for(int j = 0; j < foodLstArray.size(); j++){
 						JsonArray SFItemArray = foodLstArray.get(j).getAsJsonObject().get("items").getAsJsonArray();
 						//遍历套餐明细并且判断是否有对应的单菜品
 						for(int k = 0; k < SFItemArray.size(); k++){
 							String setfoodkey = SFItemArray.get(k).getAsJsonObject().get("foodKey").getAsString();
-							if(!foodKeys.contains(setfoodkey)){
-								String SFDetailName = SFItemArray.get(k).getAsJsonObject().get("foodName").getAsString();
-								if(foodNames.contains(SFDetailName)){
-									System.out.println("套餐" + foodName + "的明细菜品：" + SFDetailName + "缺失对应的单菜品，菜品的key为：" + setfoodkey + ",有同名的单菜品，但是foodKey不一致！！");
-								}
-								else{
-									System.out.println("套餐" + foodName + "的明细菜品：" + SFDetailName + "缺失对应的单菜品，菜品的key为：" + setfoodkey);
-								}
+							if(foodKeys.contains(setfoodkey)){
+								System.out.println("不包含" + setfoodkey);
 							}
 						}
 					}
-				}
-				else{
-					System.out.println("不是套餐！！");
 				}
 			}
 		} catch (JsonIOException e) {
